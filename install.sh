3b68062ee4db0dff1b5b153b4a3147fafe22617c
Mitchell Nielsen <mnielsen@gitlab.com>
2020-11-13 09:41:13 -0500

    Add alacritty

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# For most config files:
 stow .
+
+# For alacritty:
+alacritty_link="$HOME/.config/alacritty/alacritty.yml"
+if [ ! -L $alacritty_link ]; then
+  ln -s $PWD/alacritty.yml $alacritty_link
+fi
