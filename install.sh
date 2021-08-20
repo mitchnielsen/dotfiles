d50a14bec984f334be45b9c2eb3cde9964f3a9cc
Mitchell Nielsen <mnielsen@gitlab.com>
2021-08-20 15:00:16 -0500

    Use XDG_CONFIG directory layout

--- install.sh
+++ install.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# For most config files:
 stow .
-
-# For alacritty:
-alacritty_link="$HOME/.config/alacritty/alacritty.yml"
-if [ ! -L $alacritty_link ]; then
-  ln -s $PWD/alacritty.yml $alacritty_link
-fi
