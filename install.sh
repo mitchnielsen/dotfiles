a00ef91c48eeff4be52e19b6177133d26b5649f0
Mitchell Nielsen <mnielsen@gitlab.com>
2021-09-03 11:32:01 -0500

    Add Brewfile

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 stow .
+printf "\nSuccessfully stowed dotfiles.\n"
+
+printf "\nTo install brew dependencies, run:\n  brew install -f ~/.config/brew/Brewfile"
+printf "\nTo update the list of brew dependencies, run:\n  brew bundle dump --force -f ~/.config/brew/Brewfile"
