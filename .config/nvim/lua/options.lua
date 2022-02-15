857bf38ecc29c4e515c04a566a7fb8a54c1ba210
Mitchell Nielsen <mnielsen@gitlab.com>
2022-02-15 14:52:14 -0600

    Disable some vim built-ins

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -54,24 +54,20 @@ opt.diffopt = "vertical"
 opt.cursorline = true
 
 local disabled_built_ins = {
-  -- "netrw",
-  -- "netrwPlugin",
-  -- "netrwSettings",
-  -- "netrwFileHandlers",
-  -- "gzip",
-  -- "zip",
-  -- "zipPlugin",
-  -- "tar",
-  -- "tarPlugin",
-  -- "getscript",
-  -- "getscriptPlugin",
-  -- "vimball",
-  -- "vimballPlugin",
-  -- "2html_plugin",
-  -- "logipat",
-  -- "rrhelper",
-  -- "spellfile_plugin",
-  -- "matchit",
+  "gzip",
+  "zip",
+  "zipPlugin",
+  "tar",
+  "tarPlugin",
+  "getscript",
+  "getscriptPlugin",
+  "vimball",
+  "vimballPlugin",
+  "2html_plugin",
+  "logipat",
+  "rrhelper",
+  "spellfile_plugin",
+  "matchit",
 }
 
 for _, plugin in pairs(disabled_built_ins) do
