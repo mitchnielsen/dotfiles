9075a4c713e7fd8e04b31c2b25f733d074fb3cf6
Mitchell Nielsen <mnielsen@gitlab.com>
2022-02-24 17:06:30 +0000

    disable virtual text

--- guest/.config/nvim/lua/lsp-config.lua
+++ guest/.config/nvim/lua/lsp-config.lua
@@ -29,7 +29,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics,
   {
     underline = true,
-    virtual_text = { prefix = "" },
+    virtual_text = false,
     signs = true,
     update_in_insert = false,
   }
