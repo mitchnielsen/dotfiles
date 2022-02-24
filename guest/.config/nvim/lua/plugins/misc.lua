4927e8a84d722897ca94dd2fa6d1b9b4c150e35b
Mitchell Nielsen <mnielsen@gitlab.com>
2022-02-24 17:06:59 +0000

    add trouble config

--- guest/.config/nvim/lua/plugins/misc.lua
+++ guest/.config/nvim/lua/plugins/misc.lua
@@ -1,1 +1,8 @@
-require("trouble").setup()
+require("trouble").setup{
+  action_keys = {
+    open_split = { "<c-c>" },
+    open_vsplit = { "<c-v>" },
+  },
+  auto_preview = false,
+  auto_close = true,
+}
