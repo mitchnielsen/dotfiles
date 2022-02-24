0e40bafbfe57da3eb7a8008ba5b7bdacc3b45bbf
Mitchell Nielsen <mnielsen@gitlab.com>
2022-02-24 22:37:47 +0000

    Clean up init.lua

--- guest/.config/nvim/init.lua
+++ guest/.config/nvim/init.lua
@@ -1,29 +1,18 @@
 -- Inspiration taken from https://github.com/0xsamrath/kyoto.nvim
 
 require("globals")
-
--- autocomplete configuration
-require("plugins.compe")
-require("plugins.treesitter")
--- Langauge server configuration
-require("lsp-config")
--- general configurations
 require("options")
--- Git changes(showing in line number) configuration
-require("plugins.gitsigns")
--- trouble
-require("plugins.trouble")
--- lualine
-require("plugins.lualine")
--- lualine
+require("lsp-config")
+
+require("plugins.compe")
 require("plugins.context")
--- fzf
 require("plugins.fzf")
--- source our mappings last(may change)
-vim.cmd("source ~/.config/nvim/viml/maps.vim")
--- auto-commands
+require("plugins.gitsigns")
+require("plugins.lualine")
+require("plugins.treesitter")
+require("plugins.trouble")
+
 vim.cmd("source ~/.config/nvim/viml/autocmd.vim")
--- let statements
 vim.cmd("source ~/.config/nvim/viml/lets.vim")
--- theme
+vim.cmd("source ~/.config/nvim/viml/maps.vim")
 vim.cmd("source ~/.config/nvim/viml/theme.vim")
