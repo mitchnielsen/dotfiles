1559af0ece0f7e81d7c62fd9204cade0d55c604a
Mitchell Nielsen <mnielsen@gitlab.com>
2022-03-29 16:06:17 -0500

    Use Debian as base image
    Replaces Ubuntu with Debian as the base image.

--- guest/scripts/bootstrap_root.sh
+++ guest/scripts/bootstrap_root.sh
@@ -3,11 +3,22 @@
 # Packages
 apt-get update
 apt-get install -y \
+  autoconf \
+  bison \
+  build-essential \
   cmake \
   curl \
+  git \
+  libdb-dev \
+  libffi-dev \
+  libgdbm6 \
+  libncurses5-dev \
+  libreadline6-dev \
+  libssl-dev \
+  libyaml-dev
   stow \
-  tree \
   tmux \
+  tree \
   unzip \
   xdg-utils \
   zlib1g-dev \
@@ -15,3 +26,6 @@ apt-get install -y \
 
 # Change default shell
 chsh --shell /bin/zsh vagrant
+
+# Set locale
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen && locale-gen
