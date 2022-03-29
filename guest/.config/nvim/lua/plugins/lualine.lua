fa244392b1f613f0a31247e339cc31e121fe947a
Mitchell Nielsen <mnielsen@gitlab.com>
2022-03-29 22:42:47 +0000

    Use auto theme for lualine

--- guest/.config/nvim/lua/plugins/lualine.lua
+++ guest/.config/nvim/lua/plugins/lualine.lua
@@ -3,8 +3,9 @@ vim.o.termguicolors = true
 
 require('lualine').setup({
   options = {
-    theme = 'nightfox', -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+    theme = 'auto', -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
     component_separators = { left = '|', right = '|' },
+    globalstatus = true,
   },
   sections = {
     lualine_a = {'branch'},
