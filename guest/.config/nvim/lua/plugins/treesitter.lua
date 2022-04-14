e8085ee290280bbd58f0fc3a9cd4854a7c0fa852
Mitchell Nielsen <mnielsen@gitlab.com>
2022-04-14 11:08:14 -0500

    Specify TreeSitter parsers
    Specifies TreeSitter parsers, as "maintained" is a deprecated
    identifier.

--- guest/.config/nvim/lua/plugins/treesitter.lua
+++ guest/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
- ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+ ensure_installed = { "bash", "dockerfile", "go", "graphql", "json", "lua", "make", "ruby", "yaml" },
   highlight = {
     enable = true,
   },
