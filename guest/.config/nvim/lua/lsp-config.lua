fabbc7073f8e389e145c7557520e3d6afde1aa0b
Mitchell Nielsen <mnielsen@gitlab.com>
2022-04-18 10:13:06 -0500

    Remove old comment

--- guest/.config/nvim/lua/lsp-config.lua
+++ guest/.config/nvim/lua/lsp-config.lua
@@ -29,7 +29,6 @@ vim.fn.sign_define(
   { text = "", numhl = "LspDiagnosticsDefaultHint" }
 )
 
--- set default prefix.
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics,
   {
