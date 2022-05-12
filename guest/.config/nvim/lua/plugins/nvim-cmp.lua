add6163f05971ed60763ca3fd916e7a4dbc4cff2
Mitchell Nielsen <mnielsen@gitlab.com>
2022-05-12 15:26:25 -0500

    nvim: add snippets and fixes for completion

--- guest/.config/nvim/lua/plugins/nvim-cmp.lua
+++ guest/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -3,7 +3,16 @@ local cmp = require'cmp'
 vim.o.completeopt = "menu,menuone,noselect"
 
 cmp.setup({
-  mapping = {
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
     ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
     ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
     ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
@@ -11,9 +20,14 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'path' },
-  }
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+  }),
+  sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'path' },
+      { name = 'vsnip' },
+    }, {
+      { name = 'buffer' },
+  })
 })
