fba07931c2b9789fe7cfeee9c968f692345256b0
Mitchell Nielsen <mnielsen@gitlab.com>
2022-05-13 08:10:47 -0500

    tmux/nvim: support italics

--- guest/.config/nvim/lua/plugins/nightfox.lua
+++ guest/.config/nvim/lua/plugins/nightfox.lua
@@ -2,6 +2,10 @@ require('nightfox').setup({
   options = {
     transparent = true,    -- Disable setting background
     dim_inactive = false,   -- Non focused panes set to alternative background
+    styles = {
+      comments = "italic",
+      types = "italic",
+    },
   }
 })
 
