ed5a14bece4e9b5723d840de703b7c26520b75ab
Mitchell Nielsen <mnielsen@gitlab.com>
2022-05-18 11:30:45 -0400

    add buffers to lualine

--- guest/.config/nvim/lua/plugins/lualine.lua
+++ guest/.config/nvim/lua/plugins/lualine.lua
@@ -6,16 +6,31 @@ require('lualine').setup({
     -- globalstatus = true,
   },
   sections = {
-    lualine_a = {'branch'},
-    lualine_b = {{'diagnostics', sources = {'nvim_diagnostic'}}},
-    lualine_c = {
+    lualine_a = {
+      {
+        'buffers',
+        show_filename_only = true,
+        hide_filename_extension = false,
+        show_modified_status = true,
+        mode = 2,
+        buffers_color = {
+          active = 'lualine_a_normal',
+          inactive = 'lualine_a_inactive',
+        },
+      },
+    },
+    lualine_b = {
       {
         'filename',
-        file_status = true, -- displays file status (readonly, modified)
+        file_status = true, -- display file status (readonly, modified)
         path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
-        shorting_target = 40, -- shorten path to leave space in window
+        shorting_target = 40 -- shorten path to leave space in window
       }
     },
-    lualine_x = {'filetype'}
+    lualine_c = {'progress', 'location'},
+
+    lualine_x = {{'diagnostics', sources = {'nvim_diagnostic'}}},
+    lualine_y = {'encoding', 'fileformat', 'filetype'},
+    lualine_z = {'branch'}
   }
 })
