5622d7612788758ce56bc2b9f4241d05831ce965
Mitchell Nielsen <mnielsen@gitlab.com>
2022-06-20 14:00:28 -0400

    zsh: remove extra stow call

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,7 +2,6 @@
 
 # Symlink dotfiles
 if [ ! -f "$HOME/.zprofile" ]; then echo 'export ZDOTDIR=$HOME/.config/zsh' > "$HOME/.zprofile"; fi
-stow .config --target="$HOME/.config"
 
 # Install homebrew if needed
 command -v brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
