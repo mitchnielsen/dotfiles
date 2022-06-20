6fc2b7e6df50c94896bc4ddfbde3a675af0750b1
Mitchell Nielsen <mnielsen@gitlab.com>
2022-06-20 13:36:47 -0400

    nvim: add incremental selection

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -8,4 +8,13 @@ require'nvim-treesitter.configs'.setup {
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+        init_selection = '<CR>',
+        scope_incremental = '<CR>',
+        node_incremental = '<TAB>',
+        node_decremental = '<S-TAB>',
+    },
+  },
 }
