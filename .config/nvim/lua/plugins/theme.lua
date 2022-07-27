dc0efac3ada604464c770ae7d84fa1e4f7aa70f7
Mitchell Nielsen <mnielsen@gitlab.com>
2022-07-26 21:32:57 -0400

    nvim-tree: enable nvimtree, blankline integrations

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -6,6 +6,17 @@ catppuccin.setup({
     path = vim.fn.stdpath "cache" .. "/catppuccin",
   },
   transparent_background = true,
+  integrations = {
+    indent_blankline = {
+      enabled = true,
+      colored_indent_levels = true,
+    },
+    nvimtree = {
+      enabled = true,
+      show_root = true,
+      transparent_panel = true,
+    },
+  },
 })
 
 vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
