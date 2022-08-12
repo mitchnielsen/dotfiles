aa08c4aff5ab1ad1a1cf5d7c3bcf686e0bfd755f
Mitchell Nielsen <mnielsen@gitlab.com>
2022-08-12 14:46:43 -0400

    nvim: add vale linting

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -2,6 +2,7 @@ require("null-ls").setup({
     sources = {
         require("null-ls").builtins.completion.spell,
         require("null-ls").builtins.diagnostics.golangci_lint,
+        require("null-ls").builtins.diagnostics.vale,
     },
     defaults = {
       on_attach = {
