b092e4e9df8d937287c91387793203dfef16b131
Mitchell Nielsen <mnielsen@gitlab.com>
2022-11-03 13:35:32 -0400

    nvim: update_capabilities -> default_capabilities

--- .config/nvim/lua/lsp-config.lua
+++ .config/nvim/lua/lsp-config.lua
@@ -1,7 +1,7 @@
 local vim = vim
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 require("lsp_signature").setup({ hint_enable = false })
 
