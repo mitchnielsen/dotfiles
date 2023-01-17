66d472aed4da2bf1a93d3da2f62ce4c7f2bb9006
Mitchell Nielsen <mnielsen@gitlab.com>
2023-01-17 13:21:41 -0500

    nvim: move globals to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,8 @@
-require("globals")
 require("autocmd")
 require("maps")
 require("options")
+
+vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,6 +8,7 @@ return {
   ft = {
     "go",
     "ruby",
+    "lua",
   },
   keys = {
     {"gr", "<cmd>lua vim.lsp.buf.references()<CR>", desc = "references"},
@@ -18,6 +19,18 @@ return {
   },
   config = function()
     local vim = vim
+
+    vim.g.lsp_config = {
+      lua = {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      },
+    }
     
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
