d547802b028b3f0e5524edf09de18c07847a5798
Mitchell Nielsen <mnielsen@gitlab.com>
2023-01-27 15:00:48 -0500

    nvim: fix auto-hlsearch plugin

---
+++ .config/nvim/lua/plugins/auto-hlsearch.lua
@@ -0,0 +1,8 @@
+return {
+  'asiryk/auto-hlsearch.nvim',
+  lazy = false,
+  opts = {
+    create_commands = true,
+    remap_keys = { "/", "?", "*", "#", "n", "N" },
+  }
+}
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -6,7 +6,6 @@ return {
     'takac/vim-commandcaps', -- takes care of caps typos
     'tiagovla/scope.nvim', config = true,
     'stevearc/dressing.nvim', -- improve vim.ui interfaces
-    'asiryk/auto-hlsearch.nvim', -- remove search highlight when cursor moves
     {
       "iamcco/markdown-preview.nvim", -- markdown preview plugin for (neo)vim
       build = function() vim.fn["mkdp#util#install"]() end, -- install without yarn or npm
