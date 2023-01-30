548e26c4a29b64f58b9cedd1717a5e1d2bfb593d
Mitchell Nielsen <mnielsen@gitlab.com>
2023-01-30 15:17:45 -0500

    nvim: clean up fzf-lua config

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -23,100 +23,62 @@ return {
           wrap           = 'wrap',          -- wrap|nowrap
           layout         = 'vertical',
           winopts = {                       -- builtin previewer window options
-          number            = true,
-          relativenumber    = false,
-          cursorline        = true,
-          cursorlineopt     = 'both',
-          cursorcolumn      = false,
-          signcolumn        = 'no',
-          list              = false,
-          foldenable        = false,
-          foldmethod        = 'manual',
+            number            = true,
+            relativenumber    = false,
+            cursorline        = true,
+            cursorlineopt     = 'both',
+            cursorcolumn      = false,
+            signcolumn        = 'no',
+            list              = false,
+            foldenable        = false,
+            foldmethod        = 'manual',
+          },
         },
       },
-    },
-    keymap = {
-      -- These override the default tables completely
-      -- no need to set to `false` to disable a bind
-      -- delete or modify is sufficient
-      fzf = {
-        -- fzf '--bind=' options
-        ["ctrl-z"]      = "abort",
-        ["ctrl-d"]      = "half-page-down",
-        ["ctrl-u"]      = "half-page-up",
-        ["ctrl-a"]      = "beginning-of-line",
-        ["ctrl-e"]      = "end-of-line",
-        ["alt-a"]       = "toggle-all",
+      keymap = {
+        fzf = {
+          ["ctrl-z"]      = "abort",
+          ["ctrl-d"]      = "half-page-down",
+          ["ctrl-u"]      = "half-page-up",
+          ["ctrl-a"]      = "beginning-of-line",
+          ["ctrl-e"]      = "end-of-line",
+          ["alt-a"]       = "toggle-all",
+        },
       },
-    },
-    fzf_opts = {
-      -- options are sent as `<left>=<right>`
-      -- set to `false` to remove a flag
-      -- set to '' for a non-value flag
-      -- for raw args use `fzf_args` instead
-      ['--ansi']        = '',
-      ['--prompt']      = '> ',
-      ['--info']        = 'inline',
-      ['--height']      = '100%',
-      ['--layout']      = 'reverse',
-    },
-    previewers = {
-      bat = {
-        cmd             = "bat",
-        args            = "--style=numbers,changes --color always",
-        theme           = 'OneHalfDark', -- bat preview theme (bat --list-themes)
+      fzf_opts = {
+        ['--ansi']        = '',
+        ['--prompt']      = '> ',
+        ['--info']        = 'inline',
+        ['--height']      = '100%',
+        ['--layout']      = 'reverse',
       },
-    },
-    -- provider setup
-    files = {
-      prompt            = 'Files❯ ',
-      multiprocess      = true,           -- run command in a separate process
-      git_icons         = true,           -- show git icons?
-      file_icons        = true,           -- show file icons?
-      color_icons       = true,           -- colorize file|git icons
-      rg_opts           = '--color=always --files --no-ignore-vcs --hidden --follow --glob "!.git/*" --smart-case --line-number',
-      actions = {
-        -- set bind to 'false' to disable an action
-        -- default action opens a single selection
-        -- or sends multiple selection to quickfix
-        -- replace the default action with the below
-        -- to open all files whether single or multiple
-        -- ["default"]     = actions.file_edit,
-        ["default"]     = actions.file_edit_or_qf,
-        ["ctrl-c"]      = actions.file_split,
-        ["ctrl-v"]      = actions.file_vsplit,
-        ["ctrl-t"]      = actions.file_tabedit,
-      }
-    },
-    grep = {
-      prompt            = 'Rg❯ ',
-      input_prompt      = 'Grep For❯ ',
-      multiprocess      = true,           -- run command in a separate process
-      git_icons         = true,           -- show git icons?
-      file_icons        = true,           -- show file icons?
-      color_icons       = true,           -- colorize file|git icons
-      -- executed command priority is 'cmd' (if exists)
-      -- otherwise auto-detect prioritizes `rg` over `grep`
-      -- default options are controlled by 'rg|grep_opts'
-      -- cmd            = "rg --vimgrep",
-      rg_opts           = '--color=always --no-ignore-vcs --hidden --follow --glob "!.git/*" --smart-case --line-number',
-      grep_opts         = "--binary-files=without-match --line-number --recursive --color=auto --perl-regexp",
-      -- 'live_grep_glob' options:
-      glob_flag         = "--iglob",  -- for case sensitive globs use '--glob'
-      glob_separator    = "%s%-%-",    -- query separator pattern (lua): ' --'
-      actions = {
-        -- set bind to 'false' to disable an action
-        -- default action opens a single selection
-        -- or sends multiple selection to quickfix
-        -- replace the default action with the below
-        -- to open all files whether single or multiple
-        -- ["default"]     = actions.file_edit,
-        ["default"]     = actions.file_edit_or_qf,
-        ["ctrl-c"]      = actions.file_split,
-        ["ctrl-v"]      = actions.file_vsplit,
-        ["ctrl-t"]      = actions.file_tabedit,
-      }
-    },
-  }
-end
+      previewers = {
+        bat = {
+          cmd             = "bat",
+          args            = "--style=numbers,changes --color always",
+          theme           = 'OneHalfDark', -- bat preview theme (bat --list-themes)
+        },
+      },
+      files = {
+        rg_opts           = '--color=always --no-ignore --hidden --smart-case --line-number',
+        actions = {
+          ["default"]     = actions.file_edit_or_qf,
+          ["ctrl-s"]      = actions.file_split,
+          ["ctrl-v"]      = actions.file_vsplit,
+          ["ctrl-t"]      = actions.file_tabedit,
+        }
+      },
+      grep = {
+        prompt            = 'Rg❯ ',
+        cmd               = "rg --vimgrep",
+        rg_opts           = '--color=always --no-ignore --hidden --smart-case --line-number',
+        actions = {
+          ["default"]     = actions.file_edit_or_qf,
+          ["ctrl-s"]      = actions.file_split,
+          ["ctrl-v"]      = actions.file_vsplit,
+          ["ctrl-t"]      = actions.file_tabedit,
+        }
+      },
+    }
+  end
 }
