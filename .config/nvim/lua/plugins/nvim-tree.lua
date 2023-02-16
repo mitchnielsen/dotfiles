58e462ba7386ac8036c1d2d145527e72089097a9
Mitchell Nielsen <mnielsen@gitlab.com>
2023-02-16 10:33:13 -0500

    nvim-tree: don't ignore gitignored files

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -4,10 +4,15 @@ return {
   keys = {
     {"<leader>n", "<cmd>NvimTreeFindFileToggle<cr>", desc = "toggle"},
   },
+  -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup#opening-nvim-tree-at-neovim-startup
+  lazy = false,
   opts = {
     ignore_buffer_on_setup = true,
     filters = {
       dotfiles = false,
+    },
+    git = {
+      ignore = false,
     },
     view = {
       preserve_window_proportions = true,
