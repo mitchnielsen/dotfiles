3d5a4faeb792c536c3b94f3cc12c9174a91fa3ee
Mitchell Nielsen <mnielsen@gitlab.com>
2023-02-23 11:08:43 -0500

    nvim(telescope): support opening multiple files to qflist

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,30 +1,33 @@
 return {
   'nvim-telescope/telescope.nvim',
-  dependencies = 'nvim-lua/plenary.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-live-grep-args.nvim',
+  },
   keys = {
-    { "<leader>g", "<cmd>Telescope live_grep<CR>", desc = "live_grep" },
+    { "<leader>g", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "live_grep" },
     { "<leader>f", "<cmd>Telescope find_files<CR>", desc = "files" },
     { "<leader>b", "<cmd>Telescope buffers<CR>", desc = "grep buffers" },
     {"gr", "<cmd>Telescope lsp_references<CR>", desc = "references"},
     {"gi", "<cmd>Telescope lsp_implementations<CR>", desc = "implementation"},
     {"gd", "<cmd>Telescope lsp_definitions<CR>", desc = "definition"},
   },
-  opts = {
-    defaults = {
-      layout_config = {
-        vertical = { width = 0.5 }
+  config = function()
+    local actions = require('telescope.actions')
+    local selected_qf = {
+      ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+    }
+
+    require('telescope').setup{
+      defaults = {
+        layout_config = {
+          vertical = { width = 0.5 }
+        },
+        mappings = { -- https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/mappings.lua
+          i = selected_qf,
+          n = selected_qf,
+        }
       },
-    },
-    pickers = {
-      find_files = {
-        theme = "ivy",
-        find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-      },
-      live_grep = {
-        theme = "ivy",
-        additional_args = function()
-          return {"--hidden", "--glob", "!**/.git/*"}
-        end
       pickers = {
         find_files = {
           theme = "ivy",
@@ -37,6 +40,8 @@ return {
           end
         },
       },
-    },
-  }
+    }
+
+    require("telescope").load_extension("live_grep_args")
+  end
 }
