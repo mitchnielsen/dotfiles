2963b652bf5a2a145938ecfe4c7dc7064683550b
Mitchell Nielsen <mnielsen@gitlab.com>
2023-02-28 20:56:11 -0500

    nvim: telescope -> fzf-lua

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -2,82 +2,53 @@ return {
   "ibhagwan/fzf-lua",
   dependencies = "kyazdani42/nvim-web-devicons",
   cmd = "FzfLua",
-  -- keys = {
-  --   { "<leader>rg", "<cmd>FzfLua live_grep_glob<CR>", desc = "grep" },
-  --   { "<leader>f", "<cmd>FzfLua files<CR>", desc = "files" },
-  --   { "<leader>rG", "<cmd>FzfLua live_grep_resume<CR>", desc = "grep resume" },
-  --   { "<leader>rw", "<cmd>FzfLua grep_cword<CR>", desc = "grep cursor word" },
-  --   { "<leader>b", "<cmd>FzfLua buffers<CR>", desc = "grep buffers" },
-  -- },
-  setup = function ()
+  keys = {
+    { "<leader>g", "<cmd>FzfLua live_grep_glob<CR>", desc = "grep" },
+    { "<leader>G", "<cmd>FzfLua live_grep_resume<CR>", desc = "grep resume" },
+    { "<leader>f", "<cmd>FzfLua files<CR>", desc = "files" },
+    { "<leader>rw", "<cmd>FzfLua grep_cword<CR>", desc = "grep cursor word" },
+    { "<leader>b", "<cmd>FzfLua buffers<CR>", desc = "grep buffers" },
+    { "<leader>st", "<cmd>FzfLua git_status<CR>", desc = "git stauts" },
+  },
+  config = function ()
     local actions = require "fzf-lua.actions"
+    local sharedActions = {
+      ["default"]     = actions.file_edit_or_qf,
+      ["ctrl-s"]      = actions.file_split,
+      ["ctrl-v"]      = actions.file_vsplit,
+      ["ctrl-t"]      = actions.file_tabedit,
+    }
+
+    local rg_common_opts = '--color=always --no-ignore --hidden --smart-case --line-number --glob "!.git/*"'
+    local files_opts = rg_common_opts .. ' ' .. '--files --follow'
+
     require'fzf-lua'.setup {
       winopts = {
-        fullscreen       = false,           -- start fullscreen?
-        -- split            = "belowright new",
-        height           = 0.90,            -- window height
-        width            = 0.90,            -- window width
-        row              = 0.35,            -- window row position (0=top, 1=bottom)
-        col              = 0.50,            -- window col position (0=left, 1=right)
+        split      = 'belowright new', -- open in a bottom split
         preview = {
-          wrap           = 'wrap',          -- wrap|nowrap
-          layout         = 'vertical',
-          winopts = {                       -- builtin previewer window options
-            number            = true,
-            relativenumber    = false,
-            cursorline        = true,
-            cursorlineopt     = 'both',
-            cursorcolumn      = false,
-            signcolumn        = 'no',
-            list              = false,
-            foldenable        = false,
-            foldmethod        = 'manual',
-          },
+          wrap = 'wrap', -- wrap|nowrap
         },
       },
       keymap = {
         fzf = {
-          ["ctrl-z"]      = "abort",
-          ["ctrl-d"]      = "half-page-down",
-          ["ctrl-u"]      = "half-page-up",
-          ["ctrl-a"]      = "beginning-of-line",
-          ["ctrl-e"]      = "end-of-line",
-          ["alt-a"]       = "toggle-all",
+          ["ctrl-d"] = "half-page-down",
+          ["ctrl-u"] = "half-page-up",
+          ["ctrl-a"] = "toggle-all",
         },
-      },
-      fzf_opts = {
-        ['--ansi']        = '',
-        ['--prompt']      = '> ',
-        ['--info']        = 'inline',
-        ['--height']      = '100%',
-        ['--layout']      = 'reverse',
       },
       previewers = {
         bat = {
-          cmd             = "bat",
-          args            = "--style=numbers,changes --color always",
-          theme           = 'OneHalfDark', -- bat preview theme (bat --list-themes)
+          theme = 'OneHalfDark', -- bat --list-themes
         },
       },
       files = {
-        rg_opts           = '--color=always --no-ignore --hidden --smart-case --line-number',
-        actions = {
-          ["default"]     = actions.file_edit_or_qf,
-          ["ctrl-s"]      = actions.file_split,
-          ["ctrl-v"]      = actions.file_vsplit,
-          ["ctrl-t"]      = actions.file_tabedit,
-        }
+        rg_opts = files_opts,
+        actions = sharedActions,
       },
       grep = {
-        prompt            = 'Rg❯ ',
-        cmd               = "rg --vimgrep",
-        rg_opts           = '--color=always --no-ignore --hidden --smart-case --line-number',
-        actions = {
-          ["default"]     = actions.file_edit_or_qf,
-          ["ctrl-s"]      = actions.file_split,
-          ["ctrl-v"]      = actions.file_vsplit,
-          ["ctrl-t"]      = actions.file_tabedit,
-        }
+        prompt = 'rg❯ ',
+        rg_opts = rg_common_opts,
+        actions = sharedActions,
       },
     }
   end
