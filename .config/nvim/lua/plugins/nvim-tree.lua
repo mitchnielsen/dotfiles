35b3490191b8ba8ccfd525f67c9eb5368e492a17
Mitchell Nielsen <mnielsen@gitlab.com>
2023-06-13 19:24:48 -0500

    nvim(nvim-tree): migrate to new config

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,26 +1,33 @@
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  vim.keymap.set('n', 'i', api.node.open.horizontal, opts('Open: Horizontal Split')) -- was '<C-x>'
+  vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical Split')) -- was '<C-v'
+end
+
 return {
   'kyazdani42/nvim-tree.lua',
   dependencies = "kyazdani42/nvim-web-devicons",
   keys = {
     {"<leader>n", "<cmd>NvimTreeFindFileToggle<cr>", desc = "toggle"},
   },
-  -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup#opening-nvim-tree-at-neovim-startup
   lazy = false,
-  opts = {
-    filters = {
-      dotfiles = false,
-    },
-    git = {
-      ignore = false,
-    },
-    view = {
-      preserve_window_proportions = true,
-      mappings = {
-        list = {
-          { key = "s", action = "vsplit" },
-          { key = "i", action = "split" },
-        },
+  config = function ()
+    require('nvim-tree').setup {
+      on_attach = on_attach,
+      filters = {
+        dotfiles = false,
       },
-    },
-  }
+      git = {
+        ignore = false,
+      },
+      view = {
+        preserve_window_proportions = true,
+      }
+    }
+  end
 }
