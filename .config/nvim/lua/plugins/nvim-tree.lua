eb7f14d0bbe9d8b84351412e86481e6e8cb0bad1
Mitchell Nielsen <mnielsen@gitlab.com>
2023-07-13 12:46:31 -0500

    nvim(nvim-tree): keep netrw enabled

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -21,8 +21,9 @@ return {
   lazy = false,
   config = function ()
     -- disable netrw at the very start of your init.lua
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+    -- need to keep these enabled for :GBrowse to work
+    -- vim.g.loaded_netrw = 1
+    -- vim.g.loaded_netrwPlugin = 1
 
     -- set termguicolors to enable highlight groups
     vim.opt.termguicolors = true
