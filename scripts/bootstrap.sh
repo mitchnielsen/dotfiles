b6ec3f218bbdeb6a87c684b19911139a48c361d5
Mitchell Nielsen <mnielsen@gitlab.com>
2024-01-12 10:03:22 -0600

    bootstrap: disable MacOS icon bouncing

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -63,6 +63,7 @@ function macos_settings() {
   defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
   defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
   defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms)
+  defaults write com.apple.dock no-bouncing -bool TRUE # disable app icon bouncing
   # Save settings
   killall Dock
 }
