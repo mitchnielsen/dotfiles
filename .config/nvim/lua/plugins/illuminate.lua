d44a7296baec7a0e3de02093d7c32b09118775f8
Mitchell Nielsen <mnielsen@gitlab.com>
2024-02-14 13:37:00 -0600

    nvim(illumintate): better highlighting rules

--- .config/nvim/lua/plugins/illuminate.lua
+++ .config/nvim/lua/plugins/illuminate.lua
@@ -1,8 +1,12 @@
 return {
   'RRethy/vim-illuminate',
   config = function()
+    require('illuminate').configure({
+      min_count_to_highlight = 2,
+    })
+
     vim.api.nvim_set_hl(0, 'IlluminatedWordText', { fg="black", bg="gray" })
-    vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { fg="red", bg="green" })
-    vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { fg="red", bg="green" })
+    vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { fg="black", bg="gray" })
+    vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { fg="black", bg="gray" })
   end
 }
