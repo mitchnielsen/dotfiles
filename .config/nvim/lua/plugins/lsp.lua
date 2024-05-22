da44349d151d774f30fd78ed8d8f642be13ce767
Mitchell Nielsen <inbox@mitchn.me>
2024-05-22 15:28:40 -0500

    nvim(lsp): remove extra lua config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -22,18 +22,6 @@ return {
   config = function()
     local vim = vim
 
-    vim.g.lsp_config = {
-      lua = {
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-          },
-        },
-      },
-    }
-
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
