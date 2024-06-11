d7960c5c741d238805ef53bc35d0b73090cd3fbf
Mitchell Nielsen <inbox@mitchn.me>
2024-06-11 10:32:19 -0500

    nvim(treesitter): use_langtree, indent

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,11 +6,15 @@ return {
       ensure_installed = { "bash", "dockerfile", "go", "graphql", "json", "lua", "make", "ruby", "yaml", "hcl", "terraform" },
       highlight = {
         enable = true,
+        use_langtree = true,
       },
       rainbow = {
         enable = true,
         extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
         max_file_lines = nil, -- Do not enable for files with more than n lines, int
+      },
+      indent = {
+        enable = true,
       },
       incremental_selection = {
         enable = true,
