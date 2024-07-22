3fe62f0b6e02a65a2d54b744227197cb96557728
Mitchell Nielsen <inbox@mitchn.me>
2024-07-22 16:46:47 -0500

    nvim(theme): support multiple themes
    Add other themes to the map

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,17 +1,19 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  lazy = false,
-  config = function()
-    require('catppuccin').setup({
-      flavour = "frappe", -- latte, frappe, macchiato, mocha
-      background = {
-        light = 'latte',
-        dark = 'frappe',
-      },
-    })
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require('catppuccin').setup({
+        flavour = "frappe", -- latte, frappe, macchiato, mocha
+        background = {
+          light = 'latte',
+          dark = 'frappe',
+        },
+      })
 
-    vim.cmd.colorscheme 'catppuccin'
-  end,
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
 }
