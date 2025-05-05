b5198112b45b3f9f6681dc214e47b8727c01fd2d
Mitchell Nielsen <inbox@mitchn.me>
2025-05-05 10:24:14 -0500

    nvim-lint: source LintInfo at end
    Only way the linters will display

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -23,18 +23,6 @@ return {
       },
     })
 
-    -- Show linters for the current buffer's file type
-    vim.api.nvim_create_user_command("LintInfo", function()
-      local filetype = vim.bo.filetype
-      local linters = require("lint").linters_by_ft[filetype]
-
-      if linters then
-        print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
-      else
-        print("No linters configured for filetype: " .. filetype)
-      end
-    end, {})
-
     -- create custom filetypes for use by specific linters
     vim.filetype.add({
       pattern = {
@@ -65,5 +53,17 @@ return {
         require("lint").try_lint()
       end,
     })
+
+    -- Show linters for the current buffer's file type
+    vim.api.nvim_create_user_command("LintInfo", function()
+      local filetype = vim.bo.filetype
+      local linters = require("lint").linters_by_ft[filetype]
+
+      if linters then
+        print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+      else
+        print("No linters configured for filetype: " .. filetype)
+      end
+    end, {})
   end
 }
