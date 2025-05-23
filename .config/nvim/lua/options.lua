41f9c6583f4cbf18308de71cee7a4a2a03fa0da0
Mitchell Nielsen <inbox@mitchn.me>
2025-05-23 09:27:43 -0500

    nvim: render icons? idk

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -99,6 +99,10 @@ vim.opt.wildignore:append({
 vim.cmd([[highlight ExtraWhitespace ctermbg=red guibg=red]])
 vim.cmd([[match ExtraWhitespace /\s\+$/]])
 
+-- Show terminal how to render certain icons
+vim.cmd([[let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"]])
+vim.cmd([[let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"]])
+
 local disabled_built_ins = {
   "gzip",
   "zip",
