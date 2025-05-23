c12dd8831b2dec449e166f022ad2044a3199108d
Mitchell Nielsen <inbox@mitchn.me>
2025-05-23 09:08:07 -0500

    wezterm: file formatting

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
-local helpers = require 'helpers'
-local wezterm = require 'wezterm'
+local helpers = require("helpers")
+local wezterm = require("wezterm")
 local act = wezterm.action
 local config = {}
 
@@ -7,13 +7,13 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = 'OneHalfDark'
+config.color_scheme = "OneHalfDark"
 
 -- Match the 'cool' variant of onedark theme
 config.colors = {
-  background = '#242b38',
-  cursor_fg = '#242b38',
-  cursor_border = '#242b38',
+  background = "#242b38",
+  cursor_fg = "#242b38",
+  cursor_border = "#242b38",
 }
 
 -- wezterm ls-fonts
@@ -24,11 +24,9 @@ config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 config.enable_tab_bar = false
 config.window_decorations = 'TITLE | RESIZE'
 
-wezterm.on('format-window-title',
-  function ()
-    return 'Terminal'
-  end
-)
+wezterm.on("format-window-title", function()
+  return "Terminal"
+end)
 
 config.enable_scroll_bar = false
 config.window_padding = {
@@ -40,28 +38,28 @@ config.window_padding = {
 
 config.keys = {
   {
-    key = 'k',
-    mods = 'CMD',
-    action = act.Multiple {
-      act.SendString "clear",
-      act.SendKey { key = 'Enter' },
-    }
+    key = "k",
+    mods = "CMD",
+    action = act.Multiple({
+      act.SendString("clear"),
+      act.SendKey({ key = "Enter" }),
+    }),
   },
   {
-    key = ']',
-    mods = 'CMD|SHIFT',
-    action = act.Multiple {
-      act.SendKey { key = 'B', mods = 'CTRL' },
-      act.SendKey { key = 'N' },
-    },
+    key = "]",
+    mods = "CMD|SHIFT",
+    action = act.Multiple({
+      act.SendKey({ key = "B", mods = "CTRL" }),
+      act.SendKey({ key = "N" }),
+    }),
   },
   {
-    key = '[',
-    mods = 'CMD|SHIFT',
-    action = act.Multiple {
-      act.SendKey { key = 'B', mods = 'CTRL' },
-      act.SendKey { key = 'P' },
-    },
+    key = "[",
+    mods = "CMD|SHIFT",
+    action = act.Multiple({
+      act.SendKey({ key = "B", mods = "CTRL" }),
+      act.SendKey({ key = "P" }),
+    }),
   },
 }
 
