77d0eddab88f951bbd70f58525266d1a92951421
Mitchell Nielsen <inbox@mitchn.me>
2025-08-08 14:36:25 -0500

    nvim(lsp): apply formatting

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
-  'neovim/nvim-lspconfig',
+  "neovim/nvim-lspconfig",
   dependencies = {
-    'saghen/blink.cmp',
+    "saghen/blink.cmp",
   },
   ft = {
     "go",
@@ -13,19 +13,19 @@ return {
     "terraform",
   },
   keys = {
-    {"K", "<cmd>lua vim.lsp.buf.hover()<CR>", desc = "lsp: hover"},
-    {"gR", "<cmd>lua vim.lsp.buf.rename()<CR>", desc = "lsp: rename"}, -- default: grn
-    {"ge", "<cmd>lua vim.diagnostic.open_float()<CR>", desc = "lsp: show full error"},
-    {"]e", "<cmd>lua vim.diagnostic.goto_next()<CR>", desc = "lsp: next error"},
-    {"[e", "<cmd>lua vim.diagnostic.goto_prev()<CR>", desc = "lsp: previous error"},
-    {"gr", "<cmd>FzfLua lsp_references<CR>", desc = "lsp: references"}, -- default: grr
-    {"gi", "<cmd>FzfLua lsp_implementations<CR>", desc = "lsp: implementation"}, -- gri
-    {"gd", "<cmd>FzfLua lsp_definitions<CR>", desc = "lsp: definition"},
-    {"ga", "<cmd>FzfLua lsp_code_actions<CR>", desc = "lsp: code action"}, -- default: gra
+    { "K", "<cmd>lua vim.lsp.buf.hover()<CR>", desc = "lsp: hover" },
+    { "gR", "<cmd>lua vim.lsp.buf.rename()<CR>", desc = "lsp: rename" }, -- default: grn
+    { "ge", "<cmd>lua vim.diagnostic.open_float()<CR>", desc = "lsp: show full error" },
+    { "]e", "<cmd>lua vim.diagnostic.goto_next()<CR>", desc = "lsp: next error" },
+    { "[e", "<cmd>lua vim.diagnostic.goto_prev()<CR>", desc = "lsp: previous error" },
+    { "gr", "<cmd>FzfLua lsp_references<CR>", desc = "lsp: references" }, -- default: grr
+    { "gi", "<cmd>FzfLua lsp_implementations<CR>", desc = "lsp: implementation" }, -- gri
+    { "gd", "<cmd>FzfLua lsp_definitions<CR>", desc = "lsp: definition" },
+    { "ga", "<cmd>FzfLua lsp_code_actions<CR>", desc = "lsp: code action" }, -- default: gra
   }, -- ctrl-S for signature help in insert and select mode
   config = function()
     local vim = vim
-    local lspconfig = require('lspconfig')
+    local lspconfig = require("lspconfig")
 
     -- Disable to avoid a large log file.
     -- Set to "debug" when debugging.
@@ -39,8 +39,8 @@ return {
       lua_ls = {
         settings = {
           Lua = {
-            runtime = { version = 'LuaJIT' },
-            diagnostics = { globals = {'vim'} },
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
             workspace = { library = vim.api.nvim_get_runtime_file("", true) },
             telemetry = { enable = false },
           },
@@ -63,7 +63,7 @@ return {
       },
     }
 
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
+    local capabilities = require("blink.cmp").get_lsp_capabilities()
 
     -- Setup simple servers
     for _, server in ipairs(servers) do
@@ -78,5 +78,5 @@ return {
         capabilities = capabilities,
       }, config))
     end
-  end
+  end,
 }
