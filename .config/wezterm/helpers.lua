165d08c15955b5a915fa56bb35d3fe88160ca010
Mitchell Nielsen <inbox@mitchn.me>
2025-08-11 12:55:35 -0500

    wezterm: refactor some more

--- .config/wezterm/helpers.lua
+++ .config/wezterm/helpers.lua
@@ -1,17 +1,17 @@
 local module = {}
 
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 function module.tmux_panes(keymap)
-  for num=0,5 do
+  for num = 0, 5 do
     table.insert(keymap, {
       key = tostring(num),
-      mods = 'CMD',
-      action = act.Multiple {
-        act.SendKey { key = 'b', mods = 'CTRL' },
-        act.SendKey { key = tostring(num) },
-      }
+      mods = "CMD",
+      action = act.Multiple({
+        act.SendKey({ key = "b", mods = "CTRL" }),
+        act.SendKey({ key = tostring(num) }),
+      }),
     })
   end
 end
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,35 +1,29 @@
 local wezterm = require("wezterm")
-local helpers = require("helpers")
-local act = wezterm.action
 local config = wezterm.config_builder()
+local act = wezterm.action
 
-config.max_fps = 120
-
+-- Appearance
 config.color_scheme = "nordfox"
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.max_fps = 120
+config.enable_scroll_bar = false
+config.window_padding = {
+  left = 15,
+  right = 15,
+  top = 15,
+  bottom = 15,
+}
 
--- List available fonts:
+-- Font
 --   wezterm ls-fonts --list-system
 --   fc-list
 config.font = wezterm.font("SFMono Nerd Font")
-config.line_height = 1.25
 config.font_size = 12.0
+config.line_height = 1.25
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
-config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
-
-wezterm.on("format-window-title", function()
-  return "Terminal"
-end)
-
-config.enable_scroll_bar = false
-config.window_padding = {
-  left = 1,
-  right = 1,
-  top = 0,
-  bottom = 1,
-}
-
+-- Key bindings
 config.keys = {
   {
     key = "k",
@@ -57,6 +51,16 @@ config.keys = {
   },
 }
 
-helpers.tmux_panes(config.keys)
+-- Add bindings for changing tmux panes
+for num = 0, 5 do
+  table.insert(config.keys, {
+    key = tostring(num),
+    mods = "CMD",
+    action = act.Multiple({
+      act.SendKey({ key = "b", mods = "CTRL" }),
+      act.SendKey({ key = tostring(num) }),
+    }),
+  })
+end
 
 return config
