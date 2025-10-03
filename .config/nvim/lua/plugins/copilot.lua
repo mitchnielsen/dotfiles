c6c3517276c1528b76bb9ce5ac75443482b60347
Mitchell Nielsen <inbox@mitchn.me>
2025-10-03 12:58:55 -0500

    nvim(copilot): fix key mapping error

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -9,17 +9,6 @@ return {
         },
         suggestion = {
           enabled = false, -- defer to blink
-          auto_trigger = false,
-          hide_during_completion = true,
-          debounce = 75,
-          keymap = {
-            accept = "<C-y>",
-            accept_word = false,
-            accept_line = true,
-            next = "<C-]>",
-            prev = "<C-[>",
-            dismiss = "<ESC>",
-          },
         },
         filetypes = {
           help = false,
