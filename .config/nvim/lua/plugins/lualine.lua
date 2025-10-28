2bd4078383380c2c2c589eca7648484d7bb40927
Mitchell Nielsen <inbox@mitchn.me>
2025-10-28 11:01:27 -0500

    nvim(lualine): shift tabline for nvim-tree

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,36 +1,56 @@
 return {
   "nvim-lualine/lualine.nvim",
-  opts = {
-    options = {
-      theme = "auto", -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
-      component_separators = "-",
-      section_separators = "",
-      globalstatus = true,
-    },
-    sections = {},
-    inactive_sections = {},
-    tabline = {
-      lualine_a = {
-        {
-          "buffers",
-          show_filename_only = false,
-          hide_filename_extension = false,
-          show_modified_status = true,
-          mode = 0,
-          use_mode_colors = false,
-        },
-      },
-      lualine_b = { { "diagnostics", sources = { "nvim_diagnostic" } } },
-      lualine_c = { "diff", "navic" },
-
-      lualine_x = { "lsp_status" },
-      lualine_y = { "progress", "location" },
-      lualine_z = { "selectioncount", "searchcount" },
-    },
-    extensions = {
-      "nvim-tree",
-      "lazy",
-      "fzf",
-    },
+  dependencies = {
+    "kyazdani42/nvim-tree.lua",
   },
+  opts = {},
+  config = function()
+    local nvim_tree_shift = {
+      function()
+        len = vim.api.nvim_win_get_width(require("nvim-tree.view").get_winnr()) - 1
+        title = "Nvim-Tree"
+        left = (len - #title) / 2
+        right = len - left - #title
+
+        return string.rep(" ", left) .. title .. string.rep(" ", right)
+      end,
+      cond = require("nvim-tree.view").is_visible,
+      color = "Normal",
+    }
+
+    require("lualine").setup({
+      options = {
+        theme = "auto", -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+        component_separators = "-",
+        section_separators = "",
+        globalstatus = false,
+      },
+      sections = {},
+      inactive_sections = {},
+      tabline = {
+        lualine_a = {
+          nvim_tree_shift,
+          {
+            "buffers",
+            show_filename_only = false,
+            hide_filename_extension = false,
+            show_modified_status = true,
+            mode = 0,
+            use_mode_colors = false,
+          },
+        },
+        lualine_b = { { "diagnostics", sources = { "nvim_diagnostic" } } },
+        lualine_c = { "diff", "navic" },
+
+        lualine_x = { "lsp_status" },
+        lualine_y = { "progress", "location" },
+        lualine_z = { "selectioncount", "searchcount" },
+      },
+      extensions = {
+        "nvim-tree",
+        "lazy",
+        "fzf",
+      },
+    })
+  end,
 }
