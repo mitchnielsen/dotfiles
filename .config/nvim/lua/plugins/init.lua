9c303f246c2371f3454117a2102385e162cf5337
Mitchell Nielsen <inbox@mitchn.me>
2025-12-03 11:13:45 -0600

    nvim: move simple plugins into init.lua

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,10 +1,12 @@
 return {
   {
     -- Editor helpers
-    'takac/vim-commandcaps', -- takes care of caps typos
-    'tiagovla/scope.nvim', config = true,
-    'stevearc/dressing.nvim', -- improve vim.ui interfaces
+    "takac/vim-commandcaps", -- takes care of caps typos
+    "tiagovla/scope.nvim", -- show scope lines for context
+    "stevearc/dressing.nvim", -- improve vim.ui interfaces
+    "kevinhwang91/nvim-bqf", -- better quickfix list
+    "artempyanykh/marksman", -- LSP for markdown
 
     -- ... and any others in lua/plugins/*.lua
-  }
+  },
 }
--- .config/nvim/lua/plugins/marksman.lua
+++
@@ -1,3 +0,0 @@
-return {
-  'artempyanykh/marksman',
-}
