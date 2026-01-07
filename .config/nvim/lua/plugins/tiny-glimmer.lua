1bfa30c8d7adc23ad096a27e55b67db2aac6ffaa
Mitchell Nielsen <inbox@mitchn.me>
2026-01-06 20:59:47 -0600

    nvim: remove tiny-glimmer plugin

--- .config/nvim/lua/plugins/tiny-glimmer.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "rachartier/tiny-glimmer.nvim",
-  event = "VeryLazy",
-  priority = 10, -- Low priority to catch other plugins' keybindings
-  config = function()
-    require("tiny-glimmer").setup()
-  end,
-}
