196dd13cd88b312f91d87a4dff0a22c3bdb8837d
Mitchell Nielsen <inbox@mitchn.me>
2026-01-12 22:58:17 -0600

    nvim(markdown): use newer preview plugin

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,5 +1,20 @@
 return {
-  'iamcco/markdown-preview.nvim',
-  build = function() vim.fn["mkdp#util#install"]() end, -- install without yarn or npm
+  "brianhuster/live-preview.nvim",
   ft = "markdown",
+  keys = {
+    {
+      "<leader>mp",
+      function()
+        if vim.b.livepreview_running then
+          vim.cmd("LivePreview close %")
+          vim.b.livepreview_running = false
+        else
+          vim.cmd("LivePreview start %")
+          vim.b.livepreview_running = true
+        end
+      end,
+      ft = "markdown",
+      desc = "Toggle markdown preview",
+    },
+  },
 }
