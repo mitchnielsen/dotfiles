ac399af0ed91c482534298a707a668e867dbe9c5
Mitchell Nielsen <inbox@mitchn.me>
2026-01-26 09:57:07 -0600

    theme: koda-light

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -28,11 +28,11 @@ return {
   },
   {
     "EdenEast/nightfox.nvim",
-    lazy = false,
+    lazy = true,
     priority = 1000,
     config = function()
       require("nightfox").setup()
-      vim.cmd("colorscheme carbonfox")
+      vim.cmd("colorscheme dayfox")
     end,
   },
   {
@@ -57,5 +57,43 @@ return {
     lazy = true,
     priority = 1000,
     config = true,
+  },
+  {
+    "aktersnurra/no-clown-fiesta.nvim",
+    lazy = true,
+    priority = 1000,
+    config = function()
+      require("no-clown-fiesta").setup({
+        theme = "dark", -- supported themes are: dark, dim, light
+        transparent = false, -- Enable this to disable the bg color
+        styles = {
+          comments = {},
+          functions = {},
+          keywords = {},
+          lsp = {},
+          match_paren = {},
+          type = {},
+          variables = {},
+        },
+      })
+
+      vim.cmd.colorscheme("no-clown-fiesta")
+    end,
+  },
+  {
+    "oskarnurm/koda.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("koda").setup({
+        bold = true, -- disable bold for functions and keywords
+        italic = false, -- enable italics for comments and strings
+        transparent = false, -- enable for transparent backgrounds
+      })
+
+      vim.cmd("colorscheme koda")
+      vim.cmd("set background=light")
+      vim.cmd("highlight Cursor guibg=#000000 guifg=#000000")
+    end,
   },
 }
