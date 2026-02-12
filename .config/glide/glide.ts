21c3fcf66d1c6412fbcafee82cea66c6a2df8a70
Mitchell Nielsen <inbox@mitchn.me>
2026-02-12 12:10:05 -0600

    glide: add some more configs

--- .config/glide/glide.ts
+++ .config/glide/glide.ts
@@ -10,21 +10,21 @@ glide.keymaps.set("normal", "]b", "tab_next");
 glide.keymaps.set("normal", "[b", "tab_prev");
 glide.keymaps.set("normal", "]h", "forward");
 glide.keymaps.set("normal", "[h", "back");
+glide.keymaps.set("normal", "<leader>cr", "config_reload");
+glide.keymaps.set("normal", "<D-t", "tab_new");
+glide.keymaps.set("normal", "<D-s>", () => {
+  glide.o.native_tabs = glide.o.native_tabs === "show" ? "hide" : "show";
+});
 
-// Ignore certain sites
-const ignored_sites = ['mail.google.com', 'linear.app'];
-for (const index in ignored_sites) {
-  glide.autocmds.create("UrlEnter", {
-    hostname: ignored_sites[index],
-  }, async () => {
-    await glide.excmds.execute("mode_change ignore");
-    return () => glide.excmds.execute("mode_change normal");
-  });
-}
+glide.autocmds.create("UrlEnter", {
+  hostname: "mail.google.com",
+}, async () => {
+  glide.buf.keymaps.del("normal", "e");
+});
 
 // Picker for bookmarks
 glide.keymaps.set("normal", "<leader>b", async () => {
-  const bookmarks = await browser.bookmarks.getRecent(10);
+  const bookmarks = await browser.bookmarks.getRecent(100);
 
   glide.commandline.show({
     title: "bookmarks",
@@ -49,8 +49,7 @@ glide.keymaps.set("normal", "<leader>b", async () => {
   });
 }, { description: "Open the bookmarks picker" });
 
-// Split views
-
+// split view: create
 glide.keymaps.set(
   "normal",
   "<C-w>v",
@@ -71,6 +70,7 @@ glide.keymaps.set(
   },
 );
 
+// split view: close
 glide.keymaps.set(
   "normal",
   "<C-w>q",
@@ -82,3 +82,21 @@ glide.keymaps.set(
   },
 );
 
+// custom search engines
+const searchEnginesList = [
+  { keywords: ["ddg", "dd", "duck"], url: "https://duckduckgo.com/?q={searchTerms}", name: "DuckDuckGo", is_default: true },
+  { keywords: ["g", "google"], url: "https://www.google.com/search?q={searchTerms}", name: "Google", suggest_url: "https://suggestqueries.google.com/complete/search?client=firefox&q={searchTerms}" },
+  { keywords: ["gi"], url: "https://www.google.com/search?udm=2&q={searchTerms}", name: "Google Images", suggest_url: "https://suggestqueries.google.com/complete/search?client=firefox&q={searchTerms}" },
+  { keywords: ["y", "yt", "youtube"], url: "https://www.youtube.com/results?search_query={searchTerms}", name: "YouTube" },
+  { keywords: ["w", "wiki", "wikipedia"], url: "https://ru.wikipedia.org/wiki/Special:Search?search={searchTerms}", name: "Wikipedia" },
+  { keywords: ["gh", "github"], url: "https://github.com/search?q={searchTerms}", name: "GitHub" },
+  { keywords: ["ghp", "github:prefect"], url: "https://github.com/prefecthq/?q={searchTerms}", name: "GitHub.com/PrefectHQ" },
+];
+for (const engine of searchEnginesList) {
+  for (const keyword of engine.keywords) {
+    glide.search_engines.add({
+      name: `${keyword} [${engine.name}]`, keyword: keyword, search_url: engine.url, is_default: engine.is_default,
+      suggest_url: engine.suggest_url ?? "https://ac.duckduckgo.com/ac/?q={searchTerms}&type=list"
+    });
+  }
+}
