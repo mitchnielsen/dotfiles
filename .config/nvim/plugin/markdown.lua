0818a5787832c02b2bf1e718c231b14640735ba2
Mitchell Nielsen <inbox@mitchn.me>
2026-03-30 16:20:18 -0500

    nvim: remove vim-markdown
    new defaults gO make this unnecessary

--- .config/nvim/plugin/markdown.lua
+++ .config/nvim/plugin/markdown.lua
@@ -1,12 +1,8 @@
 vim.pack.add({
-  "https://github.com/preservim/vim-markdown",
   "https://github.com/brianhuster/live-preview.nvim",
 }, { confirm = false })
 
-vim.g.markdown_fenced_languages = { "python", "ruby", "yaml", "go" }
-vim.g.vim_markdown_folding_disabled = 1
-vim.g.vim_markdown_no_default_key_mappings = 1
-vim.keymap.set("n", "<leader>md", "<cmd>Toch<cr>", { desc = "table of content" })
+vim.keymap.set("n", "<leader>md", "gO", { desc = "table of content", remap = true })
 
 vim.keymap.set("n", "<leader>mp", function()
   if vim.b.livepreview_running then
