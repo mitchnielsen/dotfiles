e2329a5545a39df672e2dc57b5ce9078581189ed
Mitchell Nielsen <inbox@mitchn.me>
2026-08-12 12:44:11 -0500

    nvim: remove commandcaps, move bqf
    - commandcaps is already covered by mini.cmdline
    - bqf moved into a dedicated file

--- .config/nvim/plugin/utilities.lua
+++
@@ -1,4 +0,0 @@
-vim.pack.add({
-  "https://github.com/takac/vim-commandcaps", -- fix mistyped capitalised commands
-  "https://github.com/kevinhwang91/nvim-bqf", -- better quickfix list
-}, { confirm = false })
