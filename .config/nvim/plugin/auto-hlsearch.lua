912af43fbebb128743765f5f2b31c174d0147346
Mitchell Nielsen <inbox@mitchn.me>
2026-08-12 12:43:26 -0500

    nvim: use built-in hlsearch feature

--- .config/nvim/plugin/auto-hlsearch.lua
+++
@@ -1,6 +0,0 @@
-vim.pack.add({ "https://github.com/asiryk/auto-hlsearch.nvim" }, { confirm = false })
-
-require("auto-hlsearch").setup({
-  create_commands = true,
-  remap_keys = { "/", "?", "*", "#", "n", "N" },
-})
