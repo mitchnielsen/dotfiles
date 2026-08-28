0ab98ef15a604fdcad5f5427bce3473e4556265d
Mitchell Nielsen <inbox@mitchn.me>
2026-08-28 15:12:56 -0500

    nvim: remove unused theme

--- .config/nvim/plugin/00-theme.lua
+++ .config/nvim/plugin/00-theme.lua
@@ -1,12 +1,10 @@
 vim.pack.add({
-  -- Available for :colorscheme / picker
-  "https://github.com/navarasu/onedark.nvim",
-  "https://github.com/Mofiqul/vscode.nvim",
   "https://github.com/EdenEast/nightfox.nvim",
-  "https://github.com/shaunsingh/nord.nvim",
-  "https://github.com/rmehri01/onenord.nvim",
+  "https://github.com/Mofiqul/vscode.nvim",
   "https://github.com/aktersnurra/no-clown-fiesta.nvim",
+  "https://github.com/navarasu/onedark.nvim",
   "https://github.com/oskarnurm/koda.nvim",
+  "https://github.com/rmehri01/onenord.nvim",
 }, { confirm = false })
 
 require("nightfox").setup({
